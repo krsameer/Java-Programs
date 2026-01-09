e05441e9584e509d784d68788dd93a8efe4e4526
krsameer <skateboard.maniac18@gmail.com>
2026-01-10 01:06:27 +0530

    New program to convert Integer to String

---
+++ IntStringConversion.java
@@ -0,0 +1,7 @@
+public class IntStringConversion{
+    public static void main(String[] args){
+        int number=12345;
+        String str=Integer.toString(number);
+        System.out.println("Integer to String: " + str);
+    }
+}
