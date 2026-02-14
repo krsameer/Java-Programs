f7e22fca1684f6cf0bd72cfb68662f3780b16b20
krsameer <skateboard.maniac18@gmail.com>
2026-02-14 12:00:18 +0530

    Starting Arrays

---
+++ ArrayBasics/ATraversal.java
@@ -0,0 +1,12 @@
+// Array Traversal
+// Time Complexity- O(n) where n is the number of elements in the array.
+// Space Complexity- O(1) as we are using only a constant amount of extra space.
+
+public class ATraversal{
+    public static void main(String[] args) {
+        int[] arr={10,20,30,40,60,70};
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+}
