0ab734aad2e27cf7e1d8ad38f59d9bdd82a4f5df
krsameer <skateboard.maniac18@gmail.com>
2026-02-22 21:36:51 +0530

    Modified

--- FindingSecondLargest.java
+++ FindingSecondLargest.java
@@ -1,3 +1,7 @@
+// This program finds the second largest number in an array of integers. The algorithm iterates through the array, keeping track of the largest and second largest numbers found so far
+// The time complexity of this algorithm is O(n), where n is the number of elements in the array, because we need to traverse the array once to find the second largest number.
+// The space complexity is O(1) because we are using only a constant amount of extra space to store the largest and second largest numbers, regardless of the size of the input array.
+
 public class FindingSecondLargest{
     public static int findSecondLargest(int[] arr) {
         int largest=Integer.MIN_VALUE;
